b18c3b0415ee0a8cddf1913e9dd84152d8265535
Daniel Eldon <52330149+Eldon-498@users.noreply.github.com>
2024-10-03 12:38:50 +0300

    create customer controller

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,63 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
+use \Illuminate\Http\JsonResponse;
 
 class CustomerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+
+    public function index(): JsonResponse
     {
-        //
+        $customers = Customer::all();
+        return response()->json($customers);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+
+    public function store(Request $request): JsonResponse
     {
-        //
+
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:customers',
+            'phone' => 'required|numeric|digits_between:0,10',
+        ]);
+
+        $customer = Customer::create(
+        $request->only(['name', 'email', 'phone'])
+        );
+
+        return response()->json($customer,201);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
