edb0c6ab2bbeab2c7281afe51c99a6cf27e0c3ae
Daniel Eldon <52330149+Eldon-498@users.noreply.github.com>
2024-10-03 12:40:47 +0300

    middleware jwt , sanctum and access token migration table

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use \App\Http\Middleware\JwtMiddleware;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/product', [ProductController::class, 'store']);
+Route::get('/customers', [CustomerController::class, 'index'])->middleware(JwtMiddleware::class);
